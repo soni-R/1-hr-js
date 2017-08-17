c1d907564d6e0238ad6a6443eb5c621d81286c45
soni <soni.rukmaji@gmail.com>
2017-08-17 21:16:30 +0530

    change to user input text

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var variableNumber = 2;
-var secondNumber = 3;
-var variableName = 'Sam';
-var secondString = 'This is ';
 
 function changeTheText(){
 	
-	document.getElementById('paragraphText').innerHTML = secondString + variableName;
+	var changeTextVariable = document.getElementById('changeUserText').value;
+	document.getElementById('paragraphText').innerHTML = changeTextVariable;
 }
 
-function changeTheNumber(){
-	
-	document.getElementById('paragraphText2').innerHTML = secondNumber * variableNumber;
-}
+
