97c4805ce25303acd318a9d2019d9fbcee4e00b7
soni <soni.rukmaji@gmail.com>
2017-08-18 15:17:58 +0530

    calling function with in function

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-//Global variables
-var variableNumber = 2;  
-var secondNumber = 3;
-var variableName = 'Sam';
-var secondString = 'This is ';
-
-// Global variables are declared outside  function, and they can be accessed  in any function in the program. 
-// Local variables are declared inside a function, and can be used(accessed) only inside that function.
-
 function changeTheText(){
-	//local variable
+	work();
+}
+
+function work(){
 	var changeTextVariable = document.getElementById('changeUserText').value;
 	document.getElementById('paragraphText').innerHTML = changeTextVariable;
 }
-
 
