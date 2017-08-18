7f174112a0e8cfaa41ff76d32eb242fa3c4071ff
soni <soni.rukmaji@gmail.com>
2017-08-18 16:30:06 +0530

    Apply css using javascript

--- app.js
+++ app.js
@@ -2,24 +2,10 @@ var coolNumber = 3;
 
 function changeTheText()
 {
-	if(coolNumber == 1)
-	{
+		document.getElementById('paragraphText').style.color = 'maroon';
+
 		var changeTextVariable = document.getElementById('changeUserText').value;
 		document.getElementById('paragraphText').innerHTML = changeTextVariable;
-	}
-	else if(coolNumber == 2)
-	{
-		alert("pop");
-	}	
-	else if(coolNumber == 3)
-	{
-		alert("Awesome");
-	}
-	else
-	{
-		var changeTextVariable1 = "Else statement fired";
-		document.getElementById('paragraphText').innerHTML = changeTextVariable1;
-	}
 }
 
 
