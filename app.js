bfa3d5916579cc7edbe0c2ea47614a80aaec5be7
soni <soni.rukmaji@gmail.com>
2017-08-18 14:46:28 +0530

    About Variable scope

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+//Global variables
+var variableNumber = 2;  
+var secondNumber = 3;
+var variableName = 'Sam';
+var secondString = 'This is ';
+
+// Global variables are declared outside  function, and they can be accessed  in any function in the program. 
+// Local variables are declared inside a function, and can be used(accessed) only inside that function.
 
 function changeTheText(){
-	
+	//local variable
 	var changeTextVariable = document.getElementById('changeUserText').value;
 	document.getElementById('paragraphText').innerHTML = changeTextVariable;
 }
